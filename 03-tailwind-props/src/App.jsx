4bea973811ab745cf65de56237942e98fd7101e3
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2025-08-05 16:12:21 +0530

    updated

---
+++ 03-tailwind-props/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
+    </div>
+  )
+}
+
+export default App
